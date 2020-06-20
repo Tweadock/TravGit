f3a83e3cd0d338e55918a87e5030629feecc3bfd
Travis Weadock <twead87@gmail.com>
2020-06-20 16:15:48 -0400

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace TravGit
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
